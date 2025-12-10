e9bcb29d3aad2e46971d0875d4e19c8de4d9aecd
SAURAV-PANTH <panthsaurav17@gmail.com>
2025-12-10 22:11:50 +0530

    DataBaseSetUp

---
+++ src/main/java/com/phasezero/code/PhasezeroCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phasezero.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhasezeroCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhasezeroCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phasezero/code/PhasezeroCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phasezero.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhasezeroCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
